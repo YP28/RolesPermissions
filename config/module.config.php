406a04179e0c86664d2eeb72c4eaa08701db7b5b
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-11 12:54:27 +0100

    removed service di from module config

--- config/module.config.php
+++ config/module.config.php
@@ -2,11 +2,6 @@
     'service_manager' => array(
         'factories' => array(
             /*
-             * Services
-             */
-            'RoleService'                   => 'RolesPermissions\Factories\Services\RoleServiceFactory',
-
-            /*
              * Mappers
              */
             'RoleMapper'                    => 'RolesPermissions\Factories\Mappers\RoleMapperFactory',
