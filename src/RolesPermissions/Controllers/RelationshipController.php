a6f4f62903c620d6288491b4304a0133e1996ca8
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-11 15:04:19 +0100

    added relationship controller and mapper

---
+++ src/RolesPermissions/Controllers/RelationshipController.php
@@ -0,0 +1,10 @@
+<?php namespace RolesPermissions\Controllers;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class RelationshipController extends AbstractActionController
+{
+
+
+
+}
---
+++ src/RolesPermissions/Mappers/RelationshipMapper.php
@@ -0,0 +1,53 @@
+<?php namespace RolesPermissions\Mappers;
+
+use RolesPermissions\Interfaces\RoleInterface;
+use RolesPermissions\Models\Role;
+use Zend\Db\Sql\Sql;
+
+class RelationshipMapper extends MySQLMapper
+{
+
+    /**
+     * @param RoleInterface $roleable
+     * @param Role $role
+     * @return \Zend\Db\Adapter\Driver\ResultInterface
+     */
+    public function addRoleToRoleable(RoleInterface $roleable, Role $role)
+    {
+        $sql = new Sql($this->dbAdapter);
+        $insert = $sql->insert(Role::$tableToRoleable)
+            ->columns(array(
+                'role_id',
+                'roleable_type',
+                'roleable_id'
+            ))
+            ->values(array(
+                $role->getId(),
+                $roleable->getModelType(),
+                $roleable->getId()
+            ));
+
+        $stmt = $sql->prepareStatementForSqlObject($insert);
+        return $stmt->execute();
+    }
+
+    /**
+     * @param RoleInterface $roleable
+     * @param Role $role
+     * @return \Zend\Db\Adapter\Driver\ResultInterface
+     */
+    public function deleteRoleFromRoleable(RoleInterface $roleable, Role $role)
+    {
+        $sql = new Sql($this->dbAdapter);
+        $delete = $sql->delete(Role::$tableToRoleable)
+            ->where(array(
+                'role_id = ?' => $role->getId(),
+                'roleable_type = ?' => $roleable->getModelType(),
+                'roleable_id = ?' => $roleable->getId()
+            ));
+
+        $stmt = $sql->prepareStatementForSqlObject($delete);
+        return $stmt->execute();
+    }
+
+}
