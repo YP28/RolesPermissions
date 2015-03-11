851c7b6d2915d708e35b42ac1e6dfb97c7115d74
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-11 10:51:31 +0100

    added distributable configuration

--- config/module.config.php
+++ config/module.config.php
@@ -2,11 +2,6 @@
     'service_manager' => array(
         'factories' => array(
             /*
-             * Database adapter
-             */
-            'Zend\Db\Adapter\Adapter'       => 'Zend\Db\Adapter\AdapterServiceFactory',
-
-            /*
              * Services
              */
             'RoleService'                   => 'RolesPermissions\Factories\Services\RoleServiceFactory',
