e0a0fe6b41c94f7edbd57cefc474ec93b7d3614d
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-19 13:46:24 +0100

    modified test

---
+++ tests/RolesPermissionsTests/Controllers/ReachableTest.php
@@ -0,0 +1,13 @@
+<?php namespace RolesPermissionsTest\Controllers;
+
+use PHPUnit_Framework_TestCase;
+
+class PermissionControllerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testTestSuite()
+    {
+        $this->assertTrue(true);
+    }
+
+}
