1c53c59a504229e41f344d67fccd2ed98e470c8d
Yordi P. <yordipauptit@hotmail.com>
2015-03-19 16:19:46 +0100

    create instance of PermissionController to check if dependencies are loaded

--- tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
+++ tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
@@ -1,12 +1,14 @@
 <?php namespace RolesPermissionsTests\Controllers;
 
 use PHPUnit_Framework_TestCase;
+use RolesPermissions\Controllers\PermissionController;
 
 class PermissionControllerTest extends PHPUnit_Framework_TestCase
 {
 
     public function testTest()
     {
+        $pc = new PermissionController;
         $this->assertTrue(true);
     }
 
