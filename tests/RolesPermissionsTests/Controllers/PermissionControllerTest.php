5af73aca99dbd60adda12591d9bf8c139bb9e154
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-19 18:43:20 +0100

    modified travis configuration and modified testTest lol

--- tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
+++ tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
@@ -8,8 +8,9 @@ class PermissionControllerTest extends PHPUnit_Framework_TestCase
 
     public function testTest()
     {
-        $pc = new PermissionController;
-        $this->assertTrue(true);
+        $mPermissionMapper = $this->getMock('\RolesPermissions\Mappers\PermissionMapper');
+        $pc = new PermissionController($mPermissionMapper);
+        $this->assertInstanceOf('PermissionController', $pc);
     }
 
 }
