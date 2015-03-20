760d42196b873c94406d0223b9878b192ee0dc00
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-20 13:39:53 +0100

    modified mockery usage

--- tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
+++ tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
@@ -2,12 +2,13 @@
 
 use PHPUnit_Framework_TestCase;
 use RolesPermissions\Controllers\PermissionController;
+use Mockery as m;
 
 class PermissionControllerTest extends PHPUnit_Framework_TestCase
 {
     public function testTest()
     {
-        $mock = \Mockery::mock('\RolesPermissions\Mappers\PermissionMapper');
+        $mock = m::mock('\RolesPermissions\Mappers\PermissionMapper');
         $permissionController = new PermissionController($mock);
         $this->assertInstanceOf('\RolesPermissions\Controllers\PermissionController', $permissionController);
     }
