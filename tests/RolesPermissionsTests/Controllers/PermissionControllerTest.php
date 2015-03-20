208b3b7fa64fcda2a51bf3798dd6c97975272810
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-20 15:58:51 +0100

    test in unittest

--- tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
+++ tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
@@ -2,24 +2,19 @@
 
 use PHPUnit_Framework_TestCase;
 use Mockery as m;
+use RolesPermissions\Controllers\PermissionController;
 
-use RolesPermissions\Factories\Controllers\PermissionControllerFactory;
 
 class PermissionControllerTest extends PHPUnit_Framework_TestCase
 {
     public function testTest()
     {
-        /*$mock = m::mock('\RolesPermissions\Mappers\PermissionMapper');
+        $mock = m::mock('\RolesPermissions\Mappers\PermissionMapper');
         $permissionController = new PermissionController($mock);
 
         $reflectionProperty = new \ReflectionProperty($permissionController, 'permissionMapper');
         $reflectionProperty->setAccessible(true);
 
-        $this->assertInstanceOf('RolesPermissions\Mappers\PermissionMapper', $reflectionProperty->getValue($permissionController));*/
-
-        $sl = m::mock('\Zend\ServiceManager\ServiceLocatorInterface');
-        $permissionControllerFactory = new PermissionControllerFactory();
-        $permissionController = $permissionControllerFactory->createService($sl);
-        $this->assertInstanceOf('RolesPermissions\Controllers\PermissionController', $permissionController);
+        $this->assertInstanceOf('RolesPermissions\Mappers\PermissionMapper', $reflectionProperty->getValue($permissionController));
     }
 }
