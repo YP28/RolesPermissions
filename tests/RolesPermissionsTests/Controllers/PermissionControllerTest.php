40baba71dd37da90001352f709b72fb53e7e4fd8
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-20 15:33:25 +0100

    test in unittest

--- tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
+++ tests/RolesPermissionsTests/Controllers/PermissionControllerTest.php
@@ -1,19 +1,26 @@
 <?php namespace RolesPermissionsTests\Controllers;
 
 use PHPUnit_Framework_TestCase;
-use RolesPermissions\Controllers\PermissionController;
 use Mockery as m;
 
-class PermissionControllerTest extends PHPUnit_Framework_TestCase
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+
+class PermissionControllerTest extends PHPUnit_Framework_TestCase implements ServiceLocatorAwareInterface
 {
+    use ServiceLocatorAwareTrait;
+
     public function testTest()
     {
-        $mock = m::mock('\RolesPermissions\Mappers\PermissionMapper');
+        /*$mock = m::mock('\RolesPermissions\Mappers\PermissionMapper');
         $permissionController = new PermissionController($mock);
 
         $reflectionProperty = new \ReflectionProperty($permissionController, 'permissionMapper');
         $reflectionProperty->setAccessible(true);
 
-        $this->assertInstanceOf('RolesPermissions\Mappers\PermissionMapper', $reflectionProperty->getValue($permissionController));
+        $this->assertInstanceOf('RolesPermissions\Mappers\PermissionMapper', $reflectionProperty->getValue($permissionController));*/
+
+        $permissionController = $this->getServiceLocator()->get('PermissionController');
+        $this->assertInstanceOf('RolesPermissions\Controllers\PermissionController', $permissionController);
     }
 }
